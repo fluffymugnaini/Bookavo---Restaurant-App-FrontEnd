a2dcefaef5e9d46d5ff58a9fe9329530161d2b07
Stella Taylor <stellataylor10@gmail.com>
2021-03-25 10:59:59 +0000

    Button component added

--- src/Component/App/Index.js
+++ src/Component/App/Index.js
@@ -1,10 +1,23 @@
 import './App.css';
 import Header from '../Header/';
+import Button from '../Button';
 
 function App() {
+function Book(){
+alert("Cool you've booked")
+}
+
+function newRec(){
+alert("Here's a new restaurant")
+}
+
   return (
     <div className="App">
       <Header />
+      <div>
+        <Button text="Book" handleClick={Book}/>
+        <Button text="Give me another!" handleClick={newRec}/>
+      </div>
         </div>
   );
 }
---
+++ src/Component/Button/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Button({text, handleClick}) {
+    return (
+        <div>
+            <button onClick={()=>{handleClick()}}>{text}</button>
+        </div>
+    )
+}
+
+
+
+export default Button;
+
