1b94aec451b30f95701c005be74c3cc96faf9d82
Waleed Al-Rwaishan <alrwaishanw@gmail.com>
2021-03-25 10:48:45 +0000

    intial header component code

---
+++ src/Component/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Header() {
+    return (
+        <div>
+            <h1>FOOD!O</h1>
+            <h2>Experience your city through food</h2>
+        </div>
+    )
+}
+
+
+
+export default Header;
+
