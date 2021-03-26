a6c5d7b58cd0b9a86bf684681b2ea9ab29e7e05d
Stella Taylor <stellataylor10@gmail.com>
2021-03-26 11:30:45 +0000

    getting data on page

--- src/Component/RestInfo/index.js
+++ src/Component/RestInfo/index.js
@@ -1,4 +1,4 @@
-import React, {useReducer, useEffect} from 'react';
+import React, {useReducer, useEffect, useState} from 'react';
 import css from './restInfo.module.css';
 import Button from '../Button';
 
@@ -15,39 +15,52 @@ function reducer (state, action){
 }
 
 const INITIAL_REST = {
-    restaurantName:"RAFF'S REFINERY",
-    description:"If you're visiting this page, you're likely here because you're searching for a random sentence. Sometimes a random word just isn't enough, and that is where the random sentence generator comes into play. By inputting the desired number, you can make a list of as many random sentences as you want or need. Producing random sentences can be helpful in a number of different ways.",
-    openingTimes:"Opening Time: 18:00hrs",
-    closingTimes:"Closing Time: 00:00hrs",
-    phoneNumber:"+01219878787",
-    addressLine1:"61 Chapel Street",
-    area:"Central",
-    postcode:"B170X",
-    websiteURL:"www.google.com",
-    photoURL:"https://images.unsplash.com/photo-1552566626-52f8b828add9?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    restaurantName:"",
+    description:"",
+    openingTimes:"",
+    closingTimes:"",
+    phoneNumber:"",
+    addressLine1:"",
+    area:"",
+    postcode:"",
+    websiteURL:"",
+    photoURL:"",
 };
+
+// restaurantName:"",
+//     description:"If you're visiting this page, you're likely here because you're searching for a random sentence. Sometimes a random word just isn't enough, and that is where the random sentence generator comes into play. By inputting the desired number, you can make a list of as many random sentences as you want or need. Producing random sentences can be helpful in a number of different ways.",
+//     openingTimes:"Opening Time: 18:00hrs",
+//     closingTimes:"Closing Time: 00:00hrs",
+//     phoneNumber:"+01219878787",
+//     addressLine1:"61 Chapel Street",
+//     area:"Central",
+//     postcode:"B170X",
+//     websiteURL:"www.google.com",
+//     photoURL:"https://images.unsplash.com/photo-1552566626-52f8b828add9?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
 const BACKEND_URL ="https://localhost:5001/restaurants";
 
-function RestInfo({ id }) {
+function RestInfo() {
    const [restaurant, dispatch] = useReducer (reducer, INITIAL_REST);
+   const [id, setId] = useState(1);
   
    useEffect(() => {
     async function getRestaurants() {
-      let response = await fetch(`${BACKEND_URL}/1`); 
+      let response = await fetch(`${BACKEND_URL}/${id}`); 
       let data = await response.json();
       console.log(data);
      dispatch({ type: "REST", payload: data });
     console.log(restaurant);
     }
     getRestaurants();
-  }, []); 
+  }, [id]); 
    
   function Book(){
 alert("Cool you've booked")
 }
 
 function newRec(){
-alert("Here's a new restaurant")
+setId(Math.floor(Math.random()*4)+1);
+console.log(id)
 }
    
    return (
@@ -57,10 +70,10 @@ alert("Here's a new restaurant")
             <img className={css.img} src={restaurant.photoURL} alt = "restaurant image" height="600px"/>
                 <div className={css.restInfo}>
             <ul >
-                <li>{restaurant.openingTimes}</li>
-                <li>{restaurant.closingTimes}</li>
-                <li>{restaurant.phoneNumber}</li>
-                <li>{restaurant.addressLine1}</li>
+                <li>Opening Time: {restaurant.openingTimes}</li>
+                <li>Closing Time: {restaurant.closingTimes}</li>
+                <li>Phone Number: {restaurant.phoneNumber}</li>
+                <li>Address: {restaurant.addressLine1}</li>
                 <li>{restaurant.area}</li>
                 <li>{restaurant.postcode}</li>
                 </ul>
