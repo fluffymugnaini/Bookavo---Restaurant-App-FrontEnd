6daa61f52459de2b1e4548caae8d55c926ecf63c
sharmap4 <sharmapulkitmukesh@gmail.com>
2021-03-29 12:06:08 +0100

    restId in progress

--- src/Component/BookingPage/index.js
+++ src/Component/BookingPage/index.js
@@ -1,38 +1,48 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import css from './bookingPage.module.css';
+import css from "./bookingPage.module.css";
 // import DatePicker from "react-datepicker";
 
-
 function BookingPage() {
-  const { register, handleSubmit, watch, errors, formState: { isSubmitting } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    errors,
+    formState: { isSubmitting },
+  } = useForm();
 
-  const BACKEND_URL ="https://localhost:5001/bookings";
+  const BACKEND_URL = "https://localhost:5001/bookings";
 
-  const onSubmit = data => {
+  const onSubmit = (data) => {
     console.log(data);
     // POST request using fetch inside useEffect React hook
     const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ 
-          RestaurantID : 1,           //needs to pull the id from the restInfo 
-          CustomerName: data.fullName,
-          BookingDate: data.date,
-          BookingTime: data.time,
-          NumberOfPeople: parseInt(data.number),
-          CustomerMobile: data.mobile,
-          CustomerEmail: data.email})
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        RestaurantID: 1, //needs to pull the id from the restInfo
+        CustomerName: data.fullName,
+        BookingDate: data.date,
+        BookingTime: data.time,
+        NumberOfPeople: parseInt(data.number),
+        CustomerMobile: data.mobile,
+        CustomerEmail: data.email,
+      }),
     };
-    
+
     fetch(`${BACKEND_URL}`, requestOptions);
-        // .then(response => response.json())
-        // .then(data => console.log(data));
-};
+    // .then(response => response.json())
+    // .then(data => console.log(data));
+  };
+
+  function Book() {
+    alert("Cool you've booked");
+  }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-  <h1 className={css.h1}>Book a Table</h1>
+      <h1 className={css.h1}>Book a Table</h1>
       <label>Full Name:</label>
       <input name="fullName" ref={register({ required: true })} />
       {/* <DatePicker
@@ -48,9 +58,9 @@ function BookingPage() {
               dateFormat="MM-dd-yyyy h:mm"
             /> */}
 
-       <label>Date:</label>
+      <label>Date:</label>
       <input name="date" ref={register({ required: true })} />
-       <label>Time:</label>
+      <label>Time:</label>
       <input name="time" ref={register({ required: true })} />
       <label>How many people?</label>
       <select name="number" ref={register({ required: true })}>
@@ -66,19 +76,21 @@ function BookingPage() {
         <option value="8+">8+</option>
       </select>
       <label>Your Mobile Number</label>
-      <input
-        name="mobile"
-        type="mobile"
-        ref={register({ required: true})}
-      />
+      <input name="mobile" type="mobile" ref={register({ required: true })} />
       <label>Email</label>
       <input
         name="email"
         ref={register({ required: true, pattern: /^\S+@\S+$/i })}
       />
-       <input disabled={isSubmitting} type="submit" />
+      <input
+        disabled={isSubmitting}
+        type="submit"
+        onClick={() => {
+          Book();
+        }}
+      />
     </form>
   );
 }
 
-export default BookingPage
+export default BookingPage;
