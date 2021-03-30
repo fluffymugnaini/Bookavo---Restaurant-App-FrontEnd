74130d77fa4fa70d294f0e51c75fbe500fc6858b
Stella Taylor <stellataylor10@gmail.com>
2021-03-30 16:29:15 +0100

    landing page styling and router with added buttons

--- src/Component/LandingPage/index.js
+++ src/Component/LandingPage/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ShrimpBookavo from '../../../src/ShrimpBookavo.png';
 import css from './landingPage.module.css';
 import {withRouter, useHistory} from 'react-router-dom';
+import CountryButton from './CountryButton';
 
 
 function LandingPage(props) {
@@ -13,7 +14,22 @@ function LandingPage(props) {
 return (
 <div className={css.bg}>
 <img src={ShrimpBookavo} className={css.bgImage} alt="dark bg image with wok of shrimp noodles"></img>
-<button onClick={onClick} className={css.button}>BK</button>
+{/* <button onClick={onClick} className={css.button}>BK</button> */}
+<div className={css.paragraph}>
+<p className={css.p}>Choose your cuisine:</p>
+<div className={css.buttons}>
+<CountryButton onClick={onClick} text='ITALY' />
+<CountryButton onClick={onClick} text='YEMEN' />
+<CountryButton onClick={onClick} text='TRINIDAD' />
+<CountryButton onClick={onClick} text='ROMANIA' />
+<CountryButton onClick={onClick} text='INDIA' />
+{/* <button className={css.italy} onClick={onClick}>ITALY</button>
+<button className={css.italy} onClick={onClick}>YEMEN</button>
+<button className={css.italy} onClick={onClick}>TRINIDAD</button>
+<button className={css.italy} onClick={onClick}>ROMANIA</button>
+<button className={css.italy} onClick={onClick}>INDIA</button> */}
+</div>
+</div>
 </div>
 )
 };
