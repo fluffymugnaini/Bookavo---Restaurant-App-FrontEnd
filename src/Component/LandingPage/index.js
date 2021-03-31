62c3ed960350f913f0c6f2660482c540d83bd5ef
Stella Taylor <stellataylor10@gmail.com>
2021-03-31 15:30:13 +0100

    folder deletion

--- src/Component/LandingPage/index.js
+++ src/Component/LandingPage/index.js
@@ -1,47 +1,70 @@
 import React, { useReducer, useState } from "react";
-import bookavobackground2 from '../../../src/bookavobackground2.png';
-import bookavologo from '../../../src/bookavologo.png'
-import css from './landingPage.module.css';
-import {withRouter, useHistory} from 'react-router-dom';
-import CountryButton from './CountryButton';
-
+import bookavobackground2 from "../../../src/bookavobackground2.png";
+import bookavologo from "../../../src/bookavologo.png";
+import css from "./landingPage.module.css";
+import { withRouter, useHistory } from "react-router-dom";
+import CountryButton from "./CountryButton";
 
 function LandingPage(props) {
-    console.log(props);
-    const history = useHistory();
-    const onClick = (country) => {
-        history.push("/recs");
-        props.setCuisine(country)
-    }
+  console.log(props);
+  const history = useHistory();
+  const onClick = (country) => {
+    history.push("/recs");
+    props.setCuisine(country);
+  };
 
-    console.log(props.cuisine);
+  console.log(props.cuisine);
 
-return (
-<div className={css.bg}>
-
-{/* <button onClick={onClick} className={css.button}>BK</button> */}
-<div className={css.content}>
-<img src={bookavologo} className={css.logo}></img>
-<div className={css.tagline}>
-<p className = {css.tag}>EAT AROUND THE WORLD,</p>
-<p className = {css.tag}>IN YOUR CITY</p>
-</div>   
-<p className={css.p}>Choose your cuisine:</p>
-<div className={css.buttons}>
-<CountryButton onClick={()=>{onClick("italy")}} text='ITALY' />
-<CountryButton onClick={()=>{onClick("yemen")}} text='YEMEN' />
-<CountryButton onClick={()=>{onClick("trinidad")}} text='TRINIDAD' />
-<CountryButton onClick={()=>{onClick("romania")}} text='ROMANIA' />
-<CountryButton onClick={()=>{onClick("india")}} text='INDIA' />
-{/* <button className={css.italy} onClick={onClick}>ITALY</button>
+  return (
+    <div className={css.bg}>
+      {/* <button onClick={onClick} className={css.button}>BK</button> */}
+      <div className={css.content}>
+        <img src={bookavologo} className={css.logo}></img>
+        <div className={css.tagline}>
+          <p className={css.tag}>EAT AROUND THE WORLD,</p>
+          <p className={css.tag}>IN YOUR CITY</p>
+        </div>
+        <p className={css.p}>Choose your cuisine:</p>
+        <div className={css.buttons}>
+          <CountryButton
+            onClick={() => {
+              onClick("italy");
+            }}
+            text="ITALY"
+          />
+          <CountryButton
+            onClick={() => {
+              onClick("yemen");
+            }}
+            text="YEMEN"
+          />
+          <CountryButton
+            onClick={() => {
+              onClick("caribbean");
+            }}
+            text="TRINIDAD"
+          />
+          <CountryButton
+            onClick={() => {
+              onClick("romania");
+            }}
+            text="ROMANIA"
+          />
+          <CountryButton
+            onClick={() => {
+              onClick("india");
+            }}
+            text="INDIA"
+          />
+          {/* <button className={css.italy} onClick={onClick}>ITALY</button>
 <button className={css.italy} onClick={onClick}>YEMEN</button>
 <button className={css.italy} onClick={onClick}>TRINIDAD</button>
 <button className={css.italy} onClick={onClick}>ROMANIA</button>
 <button className={css.italy} onClick={onClick}>INDIA</button> */}
-</div>
-</div>
-</div>
-)
-};
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default withRouter(LandingPage);
